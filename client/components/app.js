a449581f18fe7282dd4d38920ce352a42cc20a5d
debugpoint136 <dpurushotham136@gmail.com>
2016-06-06 11:24:49 -0500

    init

---
+++ client/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="app">
+        <h3>App Component</h3>
+      </div>
+    );
+
+  }
+}
+
+export default App;
